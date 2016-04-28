f831bf708c84acbfc1abdf1a3602a6572617a976
genesisdeleon <gdeleon@cristoreyny.org>
2016-04-28 12:58:49 +0000

    we have to link the turn.js to second page

---
+++ flick.js
@@ -0,0 +1,9 @@
+<div id="book">
+  <canvas id="pageflip-canvas"></canvas>
+  <div id="pages">
+    <section>
+      <div> <!-- Any type of contents here --> </div>
+    </section>
+    <!-- More <section>'s here -->
+  </div>
+</div>
